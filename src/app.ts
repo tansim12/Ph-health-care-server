30429678ed0a97c796683b49c362c066eb51baeb
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-06 12:15:19 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { userRouter } from "./app/Modules/User/User.route";
 import { adminRouter } from "./app/Modules/Admin/Admin.route";
+import globalErrorHandler from "./app/Error-Handler/globalErrorHandler";
 
 const app: Application = express();
 app.use(cors());
@@ -18,6 +19,16 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
+
+
+
+app.all("*", (req: Request, res: Response, next) => {
+  const error = new Error(`Can't find ${req.url} on the server`);
+  next(error);
+});
+
+// global error handle
+app.use(globalErrorHandler);
 
 export default app;
 
