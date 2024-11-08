574e0e8a0d51275654fe126549d84bc49e9eabb4
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-08 11:02:49 +0600

    server ts added unrejection error handlar

--- src/app/Error-Handler/globalErrorHandler.ts
+++ src/app/Error-Handler/globalErrorHandler.ts
@@ -1,10 +1,11 @@
 import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { StatusCodes } from "http-status-codes";
+
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
-import { Server } from "http";
-import app from "./app";
 
-const port = 5000;
-
+import { Server } from 'http';
+import app from './app'
+import config from './app/config';
 async function main() {
-  const server: Server = app.listen(port, () => {
-    console.log("Sever is running on port ", port);
-  });
-}
+    const server: Server = app.listen(config.port, () => {
+        console.log("Sever is running on port ", config.port);
+    });
+
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server closed!")
+            })
+        }
+        process.exit(1);
+    };
+    process.on('uncaughtException', (error) => {
+        console.log(error);
+        exitHandler();
+    });
+
+    process.on('unhandledRejection', (error) => {
+        console.log(error);
+        exitHandler();
+    })
+};
 
 main();
