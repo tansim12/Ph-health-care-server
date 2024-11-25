dace6ff12e331a136a22df4c10d59787f99d321b
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-25 20:50:16 +0600

    create appointment

--- src/app/Modules/Appointment/Appointment.service.ts
+++ src/app/Modules/Appointment/Appointment.service.ts
@@ -1,4 +1,66 @@
-const createAppointmentDB = async (tokenEmail: string, payload: any) => {};
+import prisma from "../../shared/prisma";
+import { v4 as uuidv4 } from "uuid";
+
+const createAppointmentDB = async (tokenEmail: string, payload: any) => {
+  const { doctorId, scheduleId } = payload;
+  const patientInfo = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: tokenEmail,
+      isDeleted: false,
+    },
+  });
+
+  const doctorInfo = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: doctorId,
+      isDeleted: false,
+    },
+  });
+  const doctorScheduleInfo = await prisma.doctorSchedules.findUniqueOrThrow({
+    where: {
+      doctorId_scheduleId: {
+        doctorId,
+        scheduleId,
+      },
+      isBooked: false,
+    },
+  });
+
+  const videoCallId = uuidv4();
+
+  const result = await prisma.$transaction(async (tx) => {
+    const createAppointment = await tx.appointment.create({
+      data: {
+        patientId: patientInfo?.id,
+        doctorId,
+        scheduleId,
+        videoCallingId: videoCallId,
+      },
+      include: {
+        doctor: true,
+        doctorSchedule: true,
+        patient: true,
+        schedule: true,
+      },
+    });
+
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId,
+          scheduleId,
+        },
+        isBooked: false,
+      },
+      data: {
+        isBooked: true,
+        appointmentId: createAppointment.id,
+      },
+    });
+    return createAppointment;
+  });
+  return result;
+};
 
 export const appointmentService = {
   createAppointmentDB,
